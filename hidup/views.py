6bd081fb445abcc11e0b27013050148a3e13211d
mas-fiqi <moh.rafiqi.wahyudi@mail.com>
2023-12-04 08:29:53 +0700

    pembuatan

---
+++ hidup/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def work(request):
+  template = loader.get_template('work.html')
+  return HttpResponse(template.render())
+def single(request):
+  template = loader.get_template('single.html')
+  return HttpResponse(template.render())
+def index(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
+def contact(request):
+  template = loader.get_template('contact.html')
+  return HttpResponse(template.render())
+def about(request):
+  template = loader.get_template('about.html')
+  return HttpResponse(template.render())
+
